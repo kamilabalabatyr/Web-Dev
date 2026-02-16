a07d7e0138190723662466e26dc5791f1ca209f7
kamilabalabatyr <k_balabatyr@kbtu.kz>
2026-02-16 23:57:31 +0500

    app.ts

--- Lab4/task1/angular-tutorial/src/app/app.ts
+++ Lab4/task1/angular-tutorial/src/app/app.ts
@@ -12,14 +12,27 @@ export class User {
 @Component({
   selector: 'app-root',
   template: `
-  <section>
-    <app-user />
-  </section>
+  <div [contentEditable]="isEditable">
+    You can edit this text!
+  </div>
 `,
   imports: [User],
 })
 
 export class App {
-  protected readonly title = signal('angular-tutorial');
-  city = 'San Francisco';
+  isServerRunning = true;
 }
+
+export class App1 {
+  users = [
+    { id: 0, name: 'Sarah' },
+    { id: 1, name: 'Amy' },
+    { id: 2, name: 'Rachel' },
+    { id: 3, name: 'Jessica' },
+    { id: 4, name: 'Poornima' },
+  ];
+}
+export class App3 {
+  isEditable = true;
+}
+
